145e76db4c2712eb1bb4e67d379ea6fb3b1fb43c
ChrisReilly024 <creilly024@gmail.com>
2020-06-07 01:31:20 -0400

    valid_input created, added to getmove

--- hanoi.rb
+++ hanoi.rb
@@ -6,9 +6,26 @@ class Hanoi
     def response
         [
             'Move piece from which stack to which stack? input format #,# ex.(1,2)',
-            'Invalid input, try again'
+            'Invalid input, either or both stacks do not exist, try again'
         ]
     end
 
-    
+    def get_move
+        puts response[0]
+        move = gets.chomp.split(',').map(&:to_i)        
+        if !valid_input?(move)
+            response[1]
+            get_move
+        end
+        move
+    end
+
+    def valid_input?(move)
+        if (move[0] < 0 || move[0] > 2) || (move[1] < 0 || move[1] > 2)
+            return false
+        else
+            return true
+        end
+    end
+
 end
