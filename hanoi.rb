62ef4f9df4e3828f6bbdd9123c08168847bd02a4
ChrisReilly024 <creilly024@gmail.com>
2020-06-07 01:16:51 -0400

    response pool created

--- hanoi.rb
+++ hanoi.rb
@@ -3,5 +3,12 @@ class Hanoi
         @board = [ [1,2,3], [], [] ]
     end
 
+    def response
+        [
+            'Move piece from which stack to which stack? input format #,# ex.(1,2)',
+            'Invalid input, try again'
+        ]
+    end
+
     
 end
