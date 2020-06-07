340ca2b2134b940e975bf4ba599f5894b14e79ff
ChrisReilly024 <creilly024@gmail.com>
2020-06-07 14:54:39 -0400

    refactor, playthrough complete

--- hanoi.rb
+++ hanoi.rb
@@ -1,48 +1,49 @@
+class InitError < StandardError; end
+
 class Hanoi
+    attr_reader :stones
+    attr_accessor :stacks
     def initialize
         @board = [ [1,2,3], [], [] ]
+        @move_count = 0
+        play
     end
 
     def response
         [
-            'Move piece from which stack to which stack? input format #,# ex.(1,2)',
-            'Invalid input, either or both stacks do not exist, try again',
-            'Stack error, cannot move a piece on top of a smaller piece, try again',
-            'Stack error, move stack is empty, try again',
-            'Winner!!!'
+        'Invalid input, one or both stacks do not exist, try again', #0
+        "Invalid input, You need to move a piece from one stack to another", #1
+        'Invalid move, stack error, cannot move piece on top of a smaller piece', #2
+        "Move piece from which stack to which stack? input format should be #,# ex. 1,2", #3
+        "Winner!!", #4
+        "Do you want to play again?, y / n", #5
+        "Goodbye!", #6
+        "Invalid response", #7
+        "Start stack is empty", #8
+        "Invalid input, only one stack given"        
         ]
     end
 
-    def instructions
-        puts %Q(
-            TOWERS OF HANOI!!!
-            ---------------
-            How to play:
-                The game board consist of three stacks
-                The starting stack contains 3 pieces ordered ascendingly
-                The goal of the game is to move the pieces so that all the
-                pieces are placed in the third stack in ascending order.
-
-             Rules:
-                You cannot move a piece off the board
-                You cannot play a piece on top of another 
-                You cannot move a piece onto a stack if that 
-                stack's top piece value is lesser than moving piece
-            )
+    def print
+        p @board
     end
 
     def get_move
-        puts response[0]
-        move = gets.chomp.split(',').map(&:to_i)        
-        if !valid_input?(move)
-            response[1]
-            get_move
-        end
+        puts response[3]
+        move = gets.chomp.split(',').map(&:to_i)
+        get_move if !valid_input?(move)
         move
     end
 
-    def valid_input?(move)
-        if (move[0] < 0 || move[0] > 2) || (move[1] < 0 || move[1] > 2)
+    def valid_input?(input)
+        if input.count != 2
+            puts response[9]
+            return false
+        elsif (input[0] < 0 || input[0] > 2) || (input[1] < 0 || input[1] > 2)
+            puts response[0]
+            return false
+        elsif input[0] == input[1]
+            puts response[1]
             return false
         else
             return true
@@ -50,8 +51,20 @@ class Hanoi
     end
 
     def move_piece(move)
-        @board[move[1]].unshift(@board[move[0]].shift)
-        if !valid_move?(move)
+        if valid_move?(move)
+            @board[move[1]].unshift(@board[move[0]].shift)
+            @move_count += 1
+            return true
+        else
+            get_move
+            return false
+        end
+    end
+
+    def valid_move?(move)
+        return true if @board[move[1]].empty?
+        if @board[move[0]].first > @board[move[1]].first
+            puts response[2]
             get_move
             return false
         else
@@ -59,24 +72,42 @@ class Hanoi
         end
     end
 
-    def valid_move?(move)
-        return true if @board[move[1]].empty?
-        if @board[move[0]].empty?
-            puts response[3]
-            return false
-        elsif @board[move[0]].first > @board[move[1]].first
-            puts response[2]
-            return false
-        else
+    def win?
+        if @board[2] == [1,2,3]
+            puts response[4]
             return true
+        else
+            return false
         end
     end
-
-    def win?
-        @board[2] == [1,2,3]
+    
+    def reset
+        puts response[5]
+        input = gets.chomp.downcase
+        if input != 'y' || input != 'n'
+            puts response[7]
+            reset
+            return false
+        elsif input == 'y'
+            @board = [ [1,2,3], [], [] ]
+            play
+            return true
+        else
+            puts response[6]
+            return false
+        end
     end
-
+    
     def play
-
+        until win?
+            move = get_move
+            move_piece(move)
+            puts "Move #: @move_count"
+            print
+            win?
+        end
+        # reset
+        p 'Game Over'
+        return false
     end
 end
